08a28a9c054a08d0e673e80a9d72a31c02ac2538
admin <sp0302@126.com>
2016-10-12 09:28:29 +0800

    视频引导页_钉钉
    视频引导页_钉钉

---
+++ VideoGuard_Demo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  VideoGuard_Demo
+//
+//  Created by admin on 2016/10/12.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor=[UIColor redColor];
+    
+}
+
+
+
+
+
+@end
